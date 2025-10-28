dd47c0bbb65684657f06ad10da750191111421e1
Yael Halperin <yaelh@eca.gov.il>
2025-10-28 15:27:32 +0200

    Initial CDK workshop project

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
 
 
 app = cdk.App()
-CdkWorkshopStack(app, "CdkWorkshopStack")
+CdkWorkshopStack(app, "CdkWorkshopStack",
+    env=cdk.Environment(
+        account=app.node.try_get_context("account") or "992382459260",
+        region=app.node.try_get_context("region") or "il-central-1"
+    )
+)
 
 app.synth()
