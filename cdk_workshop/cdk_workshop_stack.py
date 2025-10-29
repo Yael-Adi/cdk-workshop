91ea9647e9b3439c24d5fbaef6c576405863231c
Yael Halperin <yaelh@eca.gov.il>
2025-10-29 13:55:36 +0200

    With Hello World lambda

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,9 +1,18 @@
 from constructs import Construct
 from aws_cdk import (
-    Stack
+    Stack,
+    aws_lambda as _lambda,
 )
 
 class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        _my_lambda = _lambda.Function(
+            self,
+            "HelloHandler",
+            runtime=_lambda.Runtime.PYTHON_3_13,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="hello.handler",
+        )   
